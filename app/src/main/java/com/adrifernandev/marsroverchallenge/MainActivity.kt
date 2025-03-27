7808c7ef27698d08bb0e230ebf77fa925a2bb8f4
adriafernandez <128298030+adrifernandev@users.noreply.github.com>
2025-03-27 09:34:45 +0100

    [feat] Enable edge-to-edge display with transparent system bars in MainActivity and using DSTheme instead of material

--- app/src/main/java/com/adrifernandev/marsroverchallenge/MainActivity.kt
+++ app/src/main/java/com/adrifernandev/marsroverchallenge/MainActivity.kt
@@ -1,47 +1,31 @@
 package com.adrifernandev.marsroverchallenge
 
+import android.graphics.Color.TRANSPARENT
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.adrifernandev.marsroverchallenge.ui.theme.MarsRoverChallengeTheme
+import com.adrifernandev.marsroverchallenge.designsystem.theme.DSTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        val lightTransparentStyle = SystemBarStyle.light(
+            scrim = TRANSPARENT,
+            darkScrim = TRANSPARENT
+        )
+
+        enableEdgeToEdge(
+            statusBarStyle = lightTransparentStyle,
+            navigationBarStyle = lightTransparentStyle
+        )
+
         setContent {
-            MarsRoverChallengeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            DSTheme {
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MarsRoverChallengeTheme {
-        Greeting("Android")
     }
 }
