30491a2122a3bd0cfe95ca976b850e526a052edf
adriafernandez <128298030+adrifernandev@users.noreply.github.com>
2025-03-28 18:08:59 +0100

    feat: Update MainScreen to display rover positions and instructions

--- presentation/src/main/java/com/adrifernandev/marsroverchallenge/presentation/ui/MainScreen.kt
+++ presentation/src/main/java/com/adrifernandev/marsroverchallenge/presentation/ui/MainScreen.kt
@@ -1,12 +1,15 @@
 package com.adrifernandev.marsroverchallenge.presentation.ui
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.adrifernandev.marsroverchallenge.presentation.viewmodel.MainViewModel
 
 @Composable
@@ -14,6 +17,8 @@ fun MainScreen(
     modifier: Modifier = Modifier,
     viewModel: MainViewModel
 ) {
+    val state by viewModel.uiState.collectAsStateWithLifecycle()
+
     Scaffold(
         modifier = modifier
     ) { paddingValues ->
@@ -22,12 +27,23 @@ fun MainScreen(
                 .fillMaxSize()
                 .padding(paddingValues)
         ) {
-            MainScreenContent()
+            MainScreenContent(state)
         }
     }
 }
 
 @Composable
-private fun MainScreenContent() {
-    Text("Hello world! I'm Main Screen")
+private fun MainScreenContent(
+    state: MainViewModel.UIState
+) {
+    val initialRoverPosition = state.initialRover?.currentPosition
+    val initialRoverDirection = state.initialRover?.currentDirection
+    val finalRoverPosition = state.finalRover?.currentPosition
+    val finalRoverDirection = state.finalRover?.currentDirection
+
+    Column {
+        Text("Rover initial position: ${initialRoverPosition?.x}X ${initialRoverPosition?.y} Y ${initialRoverDirection?.name}")
+        Text("Rover final position: ${finalRoverPosition?.x}X ${finalRoverPosition?.y} Y ${finalRoverDirection?.name}")
+        Text("Rover instructions: ${state.instructions.toString()}")
+    }
 }
