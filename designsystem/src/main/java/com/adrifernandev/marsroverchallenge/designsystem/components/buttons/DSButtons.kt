3577def3394757e8ec0532f3b4c84b7846c434ee
adriafernandez <128298030+adrifernandev@users.noreply.github.com>
2025-03-28 19:08:36 +0100

    feat: Implemented base (internal) and primary button on DS

---
+++ designsystem/src/main/java/com/adrifernandev/marsroverchallenge/designsystem/components/buttons/DSButtons.kt
@@ -0,0 +1,86 @@
+package com.adrifernandev.marsroverchallenge.designsystem.components.buttons
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.adrifernandev.marsroverchallenge.common.presentation.ui.PhonePreviews
+import com.adrifernandev.marsroverchallenge.designsystem.theme.DSTheme
+
+@Composable
+private fun DSBaseButton(
+    modifier: Modifier = Modifier,
+    buttonText: String,
+    buttonColors: ButtonColors,
+    enabled: Boolean = true,
+    onClick: () -> Unit,
+) {
+    ElevatedButton(
+        modifier = modifier
+            .height(54.dp)
+            .fillMaxWidth(),
+        onClick = onClick,
+        enabled = enabled,
+        elevation = ButtonDefaults.elevatedButtonElevation(
+            defaultElevation = DSTheme.elevations.level5,
+            pressedElevation = DSTheme.elevations.level0
+        ),
+        colors = buttonColors,
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(DSTheme.spacing.medium)
+        ) {
+            Text(
+                text = buttonText,
+                style = MaterialTheme.typography.labelLarge
+            )
+        }
+    }
+}
+
+@Composable
+fun DSPrimaryButton(
+    modifier: Modifier = Modifier,
+    buttonText: String,
+    enabled: Boolean = true,
+    onClick: () -> Unit,
+) {
+    DSBaseButton(
+        modifier = modifier,
+        buttonText = buttonText,
+        buttonColors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.secondary,
+            contentColor = MaterialTheme.colorScheme.onSecondary
+        ),
+        enabled = enabled,
+        onClick = onClick
+    )
+}
+
+@PhonePreviews
+@Composable
+private fun DSPrimaryButtonPreview() {
+    DSTheme {
+        Column(
+            modifier = Modifier.padding(DSTheme.spacing.medium),
+            verticalArrangement = Arrangement.spacedBy(DSTheme.spacing.medium)
+        ) {
+            DSPrimaryButton(
+                buttonText = "Primary button",
+                onClick = { }
+            )
+        }
+    }
+}
