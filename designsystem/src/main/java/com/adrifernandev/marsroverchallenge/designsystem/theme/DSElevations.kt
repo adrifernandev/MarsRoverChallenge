17d865bec8ac173e04faba83adc241959ecd1524
adriafernandez <128298030+adrifernandev@users.noreply.github.com>
2025-03-28 19:08:55 +0100

    feat: Implemented DSElevations and DSSpacing on DS

---
+++ designsystem/src/main/java/com/adrifernandev/marsroverchallenge/designsystem/theme/DSElevations.kt
@@ -0,0 +1,13 @@
+package com.adrifernandev.marsroverchallenge.designsystem.theme
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class DSElevations(
+    val level0: Dp = 0.dp,
+    val level1: Dp = 1.dp,
+    val level2: Dp = 3.dp,
+    val level3: Dp = 6.dp,
+    val level4: Dp = 8.dp,
+    val level5: Dp = 12.dp
+)
