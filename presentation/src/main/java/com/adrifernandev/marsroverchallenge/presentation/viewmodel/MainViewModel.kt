e95d4b958b30ab04e9dd70150eac310283296196
adriafernandez <128298030+adrifernandev@users.noreply.github.com>
2025-03-28 17:29:13 +0100

    feat: Enhance MainViewModel to manage rover instructions and loading state

--- presentation/src/main/java/com/adrifernandev/marsroverchallenge/presentation/viewmodel/MainViewModel.kt
+++ presentation/src/main/java/com/adrifernandev/marsroverchallenge/presentation/viewmodel/MainViewModel.kt
@@ -1,10 +1,14 @@
 package com.adrifernandev.marsroverchallenge.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.adrifernandev.marsroverchallenge.domain.models.Instructions
+import com.adrifernandev.marsroverchallenge.domain.models.Rover
 import com.adrifernandev.marsroverchallenge.domain.usecases.NavigateRoverUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,11 +17,41 @@ class MainViewModel @Inject constructor(
 ) : ViewModel() {
 
     data class UIState(
-        val foo: String = ""
+        val initialRover: Rover? = null,
+        val instructions: Instructions? = null,
+        val finalRover: Rover? = null,
+        val isLoading: Boolean = false,
+        val error: String? = null
     )
 
     private val _uiState = MutableStateFlow(UIState())
     val uiState = _uiState.asStateFlow()
 
+    private fun onRequestRoverInstructions() {
+        viewModelScope.launch {
+            try {
+                setLoadingState(true)
+                val roverNavigationResult = navigateRoverUseCase()
 
+                with(roverNavigationResult) {
+                    _uiState.value = _uiState.value.copy(
+                        initialRover = this.initialRover,
+                        instructions = this.instructions,
+                        finalRover = this.finalRover
+                    )
+                }
+            } catch (e: Exception) {
+                _uiState.value = _uiState.value.copy(
+                    error = e.message
+                )
+            }
+            setLoadingState(false)
+        }
+    }
+
+    private fun setLoadingState(state: Boolean) {
+        _uiState.value = _uiState.value.copy(
+            isLoading = state
+        )
+    }
 }
