5439cf46e35c06cd1208cfb78960eddd98920abf
adriafernandez <128298030+adrifernandev@users.noreply.github.com>
2025-03-28 19:00:54 +0100

    feat: Implemented new common:presentation module for common presentation utils

---
+++ common/presentation/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kover)
+}
+
+android {
+    namespace = "${Versions.APPLICATION_ID}.common.presentation"
+    compileSdk = Versions.COMPILE_SDK
+
+    defaultConfig {
+        minSdk = Versions.MIN_SDK
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = Versions.javaVersion
+        targetCompatibility = Versions.javaVersion
+    }
+    kotlinOptions {
+        jvmTarget = Versions.JVM_TARGET
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.bundles.layer.presentation)
+    implementation(platform(libs.androidx.compose.bom))
+
+    testImplementation(libs.bundles.testing.unit)
+}
