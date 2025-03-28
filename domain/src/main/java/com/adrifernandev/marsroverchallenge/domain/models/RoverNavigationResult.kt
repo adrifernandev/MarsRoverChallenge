26fb2762af2f3ee9f2e849a43a3d015950316325
adriafernandez <128298030+adrifernandev@users.noreply.github.com>
2025-03-28 17:19:07 +0100

    feat: Add RoverNavigationResult data class to represent rover navigation outcomes

---
+++ domain/src/main/java/com/adrifernandev/marsroverchallenge/domain/models/RoverNavigationResult.kt
@@ -0,0 +1,7 @@
+package com.adrifernandev.marsroverchallenge.domain.models
+
+data class RoverNavigationResult(
+    val initialRover: Rover,
+    val instructions: Instructions,
+    val finalRover: Rover
+)
