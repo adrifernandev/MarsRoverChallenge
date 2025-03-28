6b56df3ee5a296070f12fe6927d4239132799579
adriafernandez <128298030+adrifernandev@users.noreply.github.com>
2025-03-28 16:59:32 +0100

    feat: Pass MainViewModel to MainScreen in MarsRoverChallengeNavHost

--- app/src/main/java/com/adrifernandev/marsroverchallenge/ui/navigation/MarsRoverChallengeNavHost.kt
+++ app/src/main/java/com/adrifernandev/marsroverchallenge/ui/navigation/MarsRoverChallengeNavHost.kt
@@ -4,16 +4,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import com.adrifernandev.marsroverchallenge.presentation.ui.MainScreen
+import com.adrifernandev.marsroverchallenge.presentation.viewmodel.MainViewModel
 
 @Composable
 fun MarsRoverChallengeNavHost(
     modifier: Modifier = Modifier,
-    navController: NavHostController
+    navController: NavHostController,
+    mainViewModel: MainViewModel = hiltViewModel()
 ) {
     Scaffold(
         modifier = modifier
@@ -30,7 +33,9 @@ fun MarsRoverChallengeNavHost(
                 startDestination = NavigationRoutes.Main
             ) {
                 composable<NavigationRoutes.Main> {
-                    MainScreen()
+                    MainScreen(
+                        viewModel = mainViewModel
+                    )
                 }
             }
         }
