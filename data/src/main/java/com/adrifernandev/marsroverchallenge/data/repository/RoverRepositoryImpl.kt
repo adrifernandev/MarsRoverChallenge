8b5c60f0b1d3957ed0885d121a46385dd5f6903d
adriafernandez <128298030+adrifernandev@users.noreply.github.com>
2025-03-28 20:42:07 +0100

    feat: Implement RoverRepositoryImpl for fetching rover instructions with simulated network delay

---
+++ data/src/main/java/com/adrifernandev/marsroverchallenge/data/repository/RoverRepositoryImpl.kt
@@ -0,0 +1,40 @@
+package com.adrifernandev.marsroverchallenge.data.repository
+
+import com.adrifernandev.marsroverchallenge.domain.models.Direction
+import com.adrifernandev.marsroverchallenge.domain.models.Instructions
+import com.adrifernandev.marsroverchallenge.domain.models.Plateau
+import com.adrifernandev.marsroverchallenge.domain.models.Position
+import com.adrifernandev.marsroverchallenge.domain.models.Rover
+import com.adrifernandev.marsroverchallenge.domain.models.RoverInput
+import com.adrifernandev.marsroverchallenge.domain.repository.RoverRepository
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
+import javax.inject.Inject
+
+class RoverRepositoryImpl @Inject constructor() : RoverRepository {
+
+    override fun getRoverInstructions(): Flow<Result<RoverInput>> = channelFlow {
+        delay(1000) //TODO: Simulating network delay
+        send(
+            Result.success(
+                RoverInput(
+                    plateau = Plateau(
+                        topRightCornerPosition = Position(
+                            x = 5,
+                            y = 5
+                        )
+                    ),
+                    initialRover = Rover(
+                        currentPosition = Position(
+                            x = 1,
+                            y = 2
+                        ),
+                        currentDirection = Direction.N
+                    ),
+                    instructions = Instructions.fromString("LMLMLMLMM")
+                )
+            )
+        )
+    }
+}
