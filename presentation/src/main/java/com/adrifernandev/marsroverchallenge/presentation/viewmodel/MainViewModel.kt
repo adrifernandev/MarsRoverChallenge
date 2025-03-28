5acf3334deef7e24b297911adcf45b822ca2ca77
adriafernandez <128298030+adrifernandev@users.noreply.github.com>
2025-03-28 17:07:19 +0100

    feat: Integrate NavigateRoverUseCase and add UIState management in MainViewModel

--- presentation/src/main/java/com/adrifernandev/marsroverchallenge/presentation/viewmodel/MainViewModel.kt
+++ presentation/src/main/java/com/adrifernandev/marsroverchallenge/presentation/viewmodel/MainViewModel.kt
@@ -1,15 +1,23 @@
 package com.adrifernandev.marsroverchallenge.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.adrifernandev.marsroverchallenge.domain.usecases.NavigateRoverUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-
+    private val navigateRoverUseCase: NavigateRoverUseCase
 ) : ViewModel() {
 
     data class UIState(
         val foo: String = ""
     )
+
+    private val _uiState = MutableStateFlow(UIState())
+    val uiState = _uiState.asStateFlow()
+
+
 }
