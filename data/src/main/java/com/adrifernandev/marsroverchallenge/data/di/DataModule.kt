1f5681c3f1037f2bccee314502eef86b313b94a6
adriafernandez <128298030+adrifernandev@users.noreply.github.com>
2025-03-29 01:51:53 +0100

    feat: Enhance dependency injection for Rover services and repositories

--- data/src/main/java/com/adrifernandev/marsroverchallenge/data/di/DataModule.kt
+++ data/src/main/java/com/adrifernandev/marsroverchallenge/data/di/DataModule.kt
@@ -1,11 +1,16 @@
 package com.adrifernandev.marsroverchallenge.data.di
 
+import com.adrifernandev.marsroverchallenge.data.datasource.remote.RoverRemoteDataSource
+import com.adrifernandev.marsroverchallenge.data.datasource.remote.RoverRemoteDataSourceImpl
+import com.adrifernandev.marsroverchallenge.data.datasource.remote.service.RoverService
+import com.adrifernandev.marsroverchallenge.data.datasource.remote.service.RoverServiceImpl
 import com.adrifernandev.marsroverchallenge.data.repository.RoverRepositoryImpl
 import com.adrifernandev.marsroverchallenge.domain.repository.RoverRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
 import javax.inject.Singleton
 
 @Module
@@ -14,7 +19,31 @@ object DataModule {
 
     @Provides
     @Singleton
-    fun provideMarsRoverRepository(): RoverRepository {
-        return RoverRepositoryImpl()
+    fun provideMarsRoverRepository(
+        roverRemoteDataSource: RoverRemoteDataSource
+    ): RoverRepository {
+        return RoverRepositoryImpl(roverRemoteDataSource)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRoverRemoteDataSource(
+        roverService: RoverService
+    ): RoverRemoteDataSource {
+        return RoverRemoteDataSourceImpl(roverService)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRoverService(
+        json: Json
+    ): RoverService {
+        return RoverServiceImpl(json)
+    }
+
+    @Provides
+    @Singleton
+    fun providesJson(): Json {
+        return Json { ignoreUnknownKeys = true }
     }
 }
